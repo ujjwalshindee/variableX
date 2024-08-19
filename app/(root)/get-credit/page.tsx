803050e4fa79ac0ff0652ca9cd59aa9d9ed1d8fc
ujjwalshindee <ujjwalshindee@gmail.com>
2024-08-20 04:07:15 +0530

    first commit

---
+++ app/(root)/get-credit/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GetCredit = () => {
+  return (
+    <div>GetCredit</div>
+  )
+}
+
+export default GetCredit
---
+++ app/(root)/page.tsx
@@ -0,0 +1,39 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
+
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.name || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50}]}
+      />
+    </section>
+  )
+}
+
+export default Home
